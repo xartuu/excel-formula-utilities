efd4892d595d284ad7e4366b2838a5267fa65cd2
kouts <koutsaftakis@gmail.com>
2024-08-13 20:41:53 +0300

    docs: setup vue playground page

---
+++ playground/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-green-400 p-4 border text-2xl">Test test test</div>
+</template>
--- playground/index.js
+++
@@ -1,3 +0,0 @@
-const appDiv = document.getElementById('app')
-
-appDiv.innerHTML = `<h1>Excel Formula Utilities</h1>`
