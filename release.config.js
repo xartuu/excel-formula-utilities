3cc646660c05cdaa5581590ff0d7668fa4724cf3
kouts <koutsaftakis@gmail.com>
2024-08-14 14:06:21 +0300

    fix: added docs to the release assets

--- release.config.js
+++ release.config.js
@@ -20,7 +20,7 @@ export default {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'dist/**'],
+        assets: ['CHANGELOG.md', 'dist/**', 'docs/**'],
         // eslint-disable-next-line no-template-curly-in-string
         message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
